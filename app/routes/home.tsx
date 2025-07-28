bb5662b12b741ec664273272c3193623ece1dea7
Datta Panchal <panchaldatta508@gmail.com>
2025-07-28 13:04:08 +0530

    Hero Section : Designed Hero section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,18 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Ai Resume Analyzer" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+<section className="main-section" >
+  <div className="page-heading"></div>
+  <h1>Welcome to Ai Resume Analyzer</h1>
+  <h2>Get smart feedback for your dream job!</h2>
+</section>
+  </main> 
 }
